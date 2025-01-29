1d652bbaed89d68945a86cedc685a043dfcb19e9
Frasier <fsundra@gmail.com>
2025-01-30 03:07:16 +0800

    resume

---
+++ components/Hero.tsx
@@ -0,0 +1,53 @@
+'use client'
+import React from 'react'
+import { TypeAnimation } from 'react-type-animation'
+import TSParticles from './TSParticles'
+
+
+
+const Hero = () => {
+    /*
+    * Hero Section of the resume
+    * Will contain the banner and image
+    * Should fill up the whole screen on both mobile and desktop
+    */
+    return (
+        <div className="flex flex-col h-screen justify-center items-center relative">
+        <TSParticles/>
+            <div className="col-span-5 flex flex-col items-center justify-center relative z-10">
+                <h3 className="text-3xl">
+                    Hello I&lsquo;m
+                </h3>
+                <TypeAnimation
+                sequence={[
+                    'Frasier', 2000,
+                    'A University Graduate', 1000,
+                    'A Web Developer', 1000,
+                    'A Web Developer (kinda)', 1000,
+                ]}
+                wrapper="h2"
+                speed={50}
+                repeat={Infinity}
+                style={{ fontSize: '2em', textAlign: 'center' }}
+                />
+                <p className="italic text-center"> Trying to create clean, efficient code to deliver seamless user experiences </p>
+
+                <div className="flex flex-col md:flex-row justify-center md:justify-start gap-4 mt-4">
+                    <a href="https://github.com/Fozzyack" className="rounded-xl text-center border-green-500 border-2 text-green-500 px-5 py-2 hover:bg-green-500 hover:text-black hover:-translate-y-2 transition duration-500">
+                        Github
+                    </a>
+                    <a href='/resume.pdf' className='rounded-xl border-2 border-white px-5 text-center py-2 hover:bg-white hover:text-black hover:-translate-y-2 transition duration-500'>
+                        Résumé
+                    </a>
+                </div>
+            </div>
+
+            <div className="col-span-7">
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default Hero
---
+++ components/OtherProjects.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const PROJECTS = [
+	{
+		name: "Python Project Machine Learning",
+		desc: "Allows the prediction of stockmarket prices",
+		tech: "React, NodeJS, NextJS, Typescript, PostgreSQL, TailwindCSS",
+		link: "https://fin-track-xi.vercel.app/",
+	},
+]
+
+const OtherProjects = () => {
+    return (
+		<div className="flex flex-col items-center justify-center my-8">
+			<h3 className="text-3xl">Other Projects</h3>
+			<div className="flex flex-col md:flex-row mt-4">
+				{PROJECTS.map((project, index) => (
+					<div
+						key={index}
+						className="flex bg-black flex-col items-center justify-center gap-4 max-w-[350px] border-2 border-white p-4 text-center"
+					>
+						<h5 className="text-lg text-white font-bold">
+							{project.name}
+						</h5>
+						<p>{project.desc}</p>
+						<p className="font-bold">{project.tech}</p>
+						<a
+							href={project.link}
+							className="p-2 rounded-xl border-2 border-white hover:text-black hover:-translate-y-2 hover:bg-white transition duration-500"
+						>
+							<svg
+								className="w-6 h-6"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								fill="none"
+								viewBox="0 0 24 24"
+							>
+								<path
+									stroke="currentColor"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="M13.213 9.787a3.391 3.391 0 0 0-4.795 0l-3.425 3.426a3.39 3.39 0 0 0 4.795 4.794l.321-.304m-.321-4.49a3.39 3.39 0 0 0 4.795 0l3.424-3.426a3.39 3.39 0 0 0-4.794-4.795l-1.028.961"
+								/>
+							</svg>
+						</a>
+					</div>
+				))}
+            </div>
+        </div>
+    )
+}
+
+export default OtherProjects;
